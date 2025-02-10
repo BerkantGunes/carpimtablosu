77e6f74df62e34011167b9a445db8f6541f39323
Berkant Gunes <gunesberkant96@gmail.com>
2025-02-10 10:09:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpimtablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 den baslayarak 10 a kadar giden carpim tablosunu olustur.
+            for(int i = 1; i < 11 i++)
+            {
+                for(int j = 1; j<11; j++)
+                {
+                    Console.Write(i + "X" + j + "=" + i*j + "\t"); // \t tab tusuna denk geliyor o da yanyana yazdirmamizi saglar.
+                }
+            }
+        }
+    }
+}
